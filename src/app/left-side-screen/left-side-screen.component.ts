bf66e612171311d18da8b7cd66c93f9133c57312
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-09-11 22:12:00 +0800

    feat(component): :sparkles: left side done

--- src/app/left-side-screen/left-side-screen.component.ts
+++ src/app/left-side-screen/left-side-screen.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { COUNTER } from '../utilities/const/main';
 
 @Component({
   selector: 'app-left-side-screen',
   templateUrl: './left-side-screen.component.html',
-  styleUrls: ['./left-side-screen.component.scss']
+  styleUrls: ['./left-side-screen.component.scss'],
 })
 export class LeftSideScreenComponent {
-
+  counter = COUNTER;
+  processes = [
+    { windowNum: '1', queueNum: 'B0312' },
+    { windowNum: '2', queueNum: 'B0313' },
+    { windowNum: '3', queueNum: 'B0314' },
+    { windowNum: '4', queueNum: 'B0315' },
+    { windowNum: '5', queueNum: 'B0316' },
+    { windowNum: '6', queueNum: 'B0317' },
+    { windowNum: '7', queueNum: 'B0318' },
+    { windowNum: '8', queueNum: 'B0319' },
+    { windowNum: '9', queueNum: 'B0320' },
+    { windowNum: '10', queueNum: 'B0321' },
+  ];
 }
