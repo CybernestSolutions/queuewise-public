d5ef39ba2e480ff33fba101df1873c037f1b084b
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-09-11 18:56:04 +0800

    feat(component): :sparkles: make processing numbers component

--- src/app/left-side-screen/processing-numbers/processing-numbers.component.ts
+++ src/app/left-side-screen/processing-numbers/processing-numbers.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-processing-numbers',
   templateUrl: './processing-numbers.component.html',
-  styleUrls: ['./processing-numbers.component.scss']
+  styleUrls: ['./processing-numbers.component.scss'],
 })
 export class ProcessingNumbersComponent {
-
+  @Input() windowNum: string | undefined;
+  @Input() queueNum: string | undefined;
 }
