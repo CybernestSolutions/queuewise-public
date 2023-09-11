b7cc9d8f3422246d25749264c80bc3077a28e4c8
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-09-11 22:11:27 +0800

    feat(component): :sparkles: integrate different parts in main screen

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { COMPANY_NAME, NEXT } from '../utilities/const/main';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  companyName = COMPANY_NAME;
+  next = NEXT;
+  binanLogoImg = '../../assets/images/binan-logo.png';
+  bploLogoImg = '../../assets/images/bplo-logo.png';
+  backgroundImg = '../../assets/images/background.png';
 
 }
