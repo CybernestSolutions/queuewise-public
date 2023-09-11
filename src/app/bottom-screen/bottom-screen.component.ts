675a63bfd2d62feffe98215f902bdad6f0ca166b
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-09-11 22:10:49 +0800

    feat(component): :sparkles: finish the bottom part of the screen

--- src/app/bottom-screen/bottom-screen.component.ts
+++ src/app/bottom-screen/bottom-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GREETINGS, NEW, QUEUE_STAT_DASHBOARD, RENEWAL, RETIREMENT, TOTAL_QUEUES } from '../utilities/const/bottom-side-screen';
 
 @Component({
   selector: 'app-bottom-screen',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./bottom-screen.component.scss']
 })
 export class BottomScreenComponent {
-
+queueStatusDashBoard = QUEUE_STAT_DASHBOARD;
+totalQueuesString= TOTAL_QUEUES;
+newMember = NEW;
+renewal = RENEWAL;
+retirement = RETIREMENT;
+greetings = GREETINGS;
+totalQueuesInt = 157;
+renewalInt = 100;
+newMemberInt = 50;
+retirementInt = 7;
 }
---
+++ src/app/utilities/const/bottom-side-screen.ts
@@ -0,0 +1,7 @@
+export const GREETINGS =
+  'Mabuhay Biñanense! Experience the City of Life';
+export const QUEUE_STAT_DASHBOARD = 'QUEUE STATUS DASHBOARD';
+export const NEW = 'NEW';
+export const RENEWAL = 'RENEWAL';
+export const RETIREMENT = 'RETIREMENT';
+export const TOTAL_QUEUES = 'TOTAL QUEUES:';
