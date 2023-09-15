90f20e0d8123b2796d6fe3e3135929f2c42f77e2
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-09-15 14:08:50 +0800

    build(electron): :building_construction: integrate electron, electron-builder and electron-packager

---
+++ main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false,
+    },
+    autoHideMenuBar: true, // Hide the menu bar
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'out/queuewise-public-view/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.webContents.openDevTools(); // Open DevTools here
+
+  mainWindow.setFullScreen(true); // Set to false if you want to exit full screen.
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
