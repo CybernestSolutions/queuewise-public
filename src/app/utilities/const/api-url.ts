e8ab942083933b2cca22811c48f408ac25332ee0
Raymart Salvador <salvadorraymartg@gmail.com>
2025-01-02 03:09:55 +0800

    refactor: :white_check_mark: Update API URL to Latest

--- src/app/utilities/const/api-url.ts
+++ src/app/utilities/const/api-url.ts
@@ -1,3 +1,5 @@
 // export const API_URL = 'http://127.0.0.1:3000';
-export const API_URL = 'https://queuewise2-f1401624d29b.herokuapp.com';
+// export const API_URL = 'https://queuewise2-f1401624d29b.herokuapp.com';
 // export const API_URL = 'https://queuewise-get-8864d7ddf648.herokuapp.com';
+
+export const API_URL = 'https://queuewiseserver.azurewebsites.net';
