b2bffc3e9da614b53a1cd291e311274613bb917a
raymartsalvador <salvadorraymartg@gmail.com>
2026-01-02 15:33:47 +0800

    fix(api-url): :bug: update API URL to correct endpoint

--- src/app/utilities/const/api-url.ts
+++ src/app/utilities/const/api-url.ts
@@ -2,4 +2,4 @@
 // export const API_URL = 'https://queuewise2-f1401624d29b.herokuapp.com';
 // export const API_URL = 'https://queuewise-get-8864d7ddf648.herokuapp.com';
 
-export const API_URL = 'https://queuewiseserver.azurewebsites.net';
+export const API_URL = 'https://queuewise-api-2026.azurewebsites.net';
